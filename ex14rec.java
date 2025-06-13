415dc37e0075c7138906fc9b3027220fb2454086
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-06-13 20:38:32 -0300

    Update ex14rec.java

--- ex14rec.java
+++ ex14rec.java
@@ -6,40 +6,40 @@ public class ex14rec {
 
         String nomeMes;
         switch (mes) {
-            case '1':
+            case 1:
                 JOptionPane.showMessageDialog(null, "Janeiro");
                 break;
-            case '2':
+            case 2:
                 JOptionPane.showMessageDialog(null, "Fevereiro");
                 break;
-            case '3':
+            case 3:
                 JOptionPane.showMessageDialog(null, "Marco");
                 break;
-            case '4':
+            case 4:
                 JOptionPane.showMessageDialog(null, "Abriu");
                 break;
-            case '5':
+            case 5:
                 JOptionPane.showMessageDialog(null, "Maio");
                 break;
-            case '6':
+            case 6:
                 JOptionPane.showMessageDialog(null, "Junho");
                 break;
-            case '7':
+            case 7:
                 JOptionPane.showMessageDialog(null, "Julho");
                 break;
-            case '8':
+            case 8:
                 JOptionPane.showMessageDialog(null, "Agosto");
                 break;
-            case '9':
+            case 9:
                 JOptionPane.showMessageDialog(null, "Setembro");
                 break;
-            case "10":
+            case 10:
                 JOptionPane.showMessageDialog(null, "Outubro");
                 break;
-            case "11":
+            case 11:
                 JOptionPane.showMessageDialog(null, "Novembro");
                 break;
-            case "12":
+            case 12:
                 JOptionPane.showMessageDialog(null, "Dezembro");
                 break;
             default:
